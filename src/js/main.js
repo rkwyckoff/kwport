56a010cf0fc556bceb363b8f595ad0863f03dc84
Kurt Wyckoff <kurtitservices@gmail.com>
2017-04-25 07:54:12 -0400

    few changes to Jr. title

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 import angular from 'angular';
 import 'angular-ui-router';
- import 'angular-cookies';
- import 'angular-scroll';
+import 'angular-cookies';
+import 'angular-scroll';
 
 import Config from './config';
-import SERVER from './server'
+import SERVER from './server';
 import setup from './setup';
 import LayoutController from './controllers/layout';
 
@@ -13,4 +13,4 @@ angular
   .config(Config)
   .run(setup)
   .constant('SERVER', SERVER)
-  .controller('LayoutController', LayoutController)
+  .controller('LayoutController', LayoutController);
